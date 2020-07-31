1917620e7643644deab52ad1e257d013f2498fe1
João Bispo <vitor.vi10@hotmail.com>
2020-07-31 18:10:58 -0300

    Slider :lipstick:

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -8,12 +8,27 @@ import { Link } from 'react-router-dom';
 function CadastroCategoria() {
     return(
         <PageDefault>
-            <h1>Cadastro de Categoria</h1>
-
-            <Link to="/">
-                Ir para Home
-            </Link>
-        </PageDefault>
+        <h1>Cadastro de Categoria</h1>
+  
+        <form>
+  
+          <label>
+            Nome da Categoria:
+            <input
+              type="text"
+            />
+          </label>
+  
+          <button>
+            Cadastrar
+          </button>
+        </form>
+  
+  
+        <Link to="/">
+          Ir para home
+        </Link>
+      </PageDefault>
     )
 }
 
