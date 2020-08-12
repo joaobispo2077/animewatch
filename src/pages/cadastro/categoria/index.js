a901f33f9d97729909872e3daf69267b44e8505e
João Bispo <vitor.vi10@hotmail.com>
2020-08-12 12:01:17 -0300

    fix: URL back-end

--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -29,7 +29,9 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    const URL = 'http://localhost:8080/categorias';
+    const URL = window.location.hostname.includes('localhost')
+      ? 'http://localhost:3000:8080'
+      : 'https://anime-watch.herokuapp.com/categorias';
     fetch(URL)
       .then(async (res) => {
         const answer = await res.json();
