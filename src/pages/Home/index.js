ca0f9ef1321f1f8bae0b62acc47f756cdf878191
João Bispo <vitor.vi10@hotmail.com>
2020-08-12 18:48:11 -0300

    fix: update home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,23 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import Menu from '../../components/Menu';
+// import Menu from '../../components/Menu';
 
 // import dadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
-import Footer from '../../components/Footer';
+// import Footer from '../../components/Footer';
 import categoriasRepository from '../../repositories/categorias';
+import PageDefault from '../../components/PageDefault';
 
 function Home() {
-
-  const [dadosIniciais, setDadosIniciais] = useState({
-    categorias: [],
-  });
+  const [dadosIniciais, setDadosIniciais] = useState([]);
 
   useEffect(() => {
     categoriasRepository.getAllWithVideos()
       .then((categoriesWithVideos) => {
         // eslint-disable-next-line no-console
         console.log(categoriesWithVideos);
+        setDadosIniciais(categoriesWithVideos);
       }).catch((err) => {
         // eslint-disable-next-line no-console
         console.log(err.message);
@@ -25,54 +24,66 @@ function Home() {
   }, []);
 
   return (
+    <PageDefault paddingAll={0}>
+      {dadosIniciais.length === 0 && (<div>Loading...</div>)}
 
-    <div>
-      <div>
-        <Menu />
-      </div>
-      <div style={{ background: '#141414' }}>
-        <Menu />
+      {dadosIniciais.length >= 1 && (
+        <>
+          <BannerMain
+            videoTitle={dadosIniciais[0].videos[0].titulo}
+            url={dadosIniciais[0].videos[0].url}
+            videoDescription="Compartilhe com a comunidade seus edits, memes, soundtracks, lo-fi versions, episódios, trailers e muito mais que você gostar sobre animes!"
+          />
 
-        {/* <BannerMain
-          videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
-          url={dadosIniciais.categorias[0].videos[0].url}
-          videoDescription="Compartilhe com a comunidade seus edits, memes, soundtracks, lo-fi versions, episódios, trailers e muito mais que você gostar sobre animes!"
-        />
+          <Carousel
+            ignoreFirstVideo
+            category={dadosIniciais[0]}
+          />
 
-        <Carousel
-          ignoreFirstVideo
-          category={dadosIniciais.categorias[0]}
-        />
+          <Carousel
+            category={dadosIniciais[1]}
+          />
+        </>
+      )}
 
-        <Carousel
-          category={dadosIniciais.categorias[1]}
-        />
+      {/* <BannerMain
+            videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+            url={dadosIniciais.categorias[0].videos[0].url}
+            videoDescription="Compartilhe com a comunidade seus edits, memes, soundtracks, lo-fi versions, episódios, trailers e muito mais que você gostar sobre animes!"
+          />
 
-        <Carousel
-          category={dadosIniciais.categorias[2]}
-        />
+          <Carousel
+            ignoreFirstVideo
+            category={dadosIniciais.categorias[0]}
+          />
 
-        <Carousel
-          category={dadosIniciais.categorias[3]}
-        />
+          <Carousel
+            category={dadosIniciais.categorias[1]}
+          />
 
-        <Carousel
-          category={dadosIniciais.categorias[4]}
-        />
+          <Carousel
+            category={dadosIniciais.categorias[2]}
+          />
 
-        <Carousel
-          category={dadosIniciais.categorias[5]}
-        />
-        <Carousel
-          category={dadosIniciais.categorias[6]}
-        />
-        <Carousel
-          category={dadosIniciais.categorias[7]}
-        /> */}
+          <Carousel
+            category={dadosIniciais.categorias[3]}
+          />
 
-        <Footer />
-      </div>
-    </div>
+          <Carousel
+            category={dadosIniciais.categorias[4]}
+          />
+
+          <Carousel
+            category={dadosIniciais.categorias[5]}
+          />
+          <Carousel
+            category={dadosIniciais.categorias[6]}
+          />
+          <Carousel
+            category={dadosIniciais.categorias[7]}
+          /> */}
+
+    </PageDefault>
 
   );
 }
