b520764abe0b27c80a9b4298268ec24f4cc8ad1e
João Bispo <vitor.vi10@hotmail.com>
2020-09-02 14:23:09 -0300

    chore: small details & prepare new loader :paperclip:

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,9 +4,9 @@ import { FooterBase } from './styles';
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
+      {/* <a href="https://www.alura.com.br/">
         <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
-      </a>
+      </a> */}
       <p>
         Orgulhosamente criado durante a
         {' '}
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -4,10 +4,10 @@ import styled from 'styled-components';
 export const FooterBase = styled.footer`
   background: var(--black);
   border-top: 2px solid var(--primary);
-  padding-left: 16px;
-  padding-right: 16px;
-  padding-top: 32px;
-  padding-bottom: 32px;
+  padding-left: 8px;
+  padding-right: 8px;
+  padding-top: 16px;
+  padding-bottom: 16px;
   color: var(--white);
   text-align: center;
   @media (max-width: 800px) {
